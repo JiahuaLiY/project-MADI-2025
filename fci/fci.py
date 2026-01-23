f915a559eba349eb492cf9abb787dfb49dc3adfe
JiahuaLiY <jiahua.li.y@gmail.com>
2026-01-23 14:56:23 +0100

    add rule9-10

--- fci/fci.py
+++ fci/fci.py
@@ -40,6 +40,11 @@ def isSpouse(pag: nx.Graph, x: str, y: str) -> bool:
     # Has x <-> y ?
     return hasEndpoint(pag, x, y, Endpoint.ARROW, Endpoint.ARROW)
 
+def isPDEdge(pag: nx.Graph, x: str, y: str) -> bool:
+    xyData = pag.get_edge_data(x, y)
+    return  (xyData[x] == Endpoint.CIRCLE or xyData[x] == Endpoint.TAIL) and \
+            (xyData[y] == Endpoint.CIRCLE or xyData[y] == Endpoint.ARROW)
+
 
 
 def getPDSep(pag: nx.Graph, x: str) -> set:
@@ -81,9 +86,9 @@ def getDiscriminatingPath(pag: nx.Graph, x: str, z: str, y: str) -> list[str]:
     queue = deque()
     queue.append(x)
 
-    visited = set(pag.nodes)
-    visited.remove(z)
-    visited.remove(y)
+    visited = set()
+    # visited.remove(z)
+    # visited.remove(y)
 
     links = { u: None for u in pag.nodes }
     links[z] = y
@@ -92,9 +97,9 @@ def getDiscriminatingPath(pag: nx.Graph, x: str, z: str, y: str) -> list[str]:
     while queue:
         u = queue.popleft()
 
-        if u not in visited:
+        if u in visited:
             continue
-        visited.remove(u)
+        visited.add(u)
 
         for v in pag.neighbors(u):
             if v == x or v == z or v == y:
@@ -131,13 +136,13 @@ def getUncoveredCirclePath(pag: nx.Graph, x: str, y: str) -> Generator[list[str]
         visited[v] = True
 
         if v == y:
-            valid = True
+            isUncoveredCirclePath = True
             for i in range(len(path) - 1):
                 ui, vi = path[i], path[i + 1]
                 if not hasEndpoint(pag, ui, vi, Endpoint.CIRCLE, Endpoint.CIRCLE):
-                    valid = False
+                    isUncoveredCirclePath = False
                     break
-            if valid:
+            if isUncoveredCirclePath:
                 yield path[:]
 
         for w in pag.neighbors(v):
@@ -145,6 +150,24 @@ def getUncoveredCirclePath(pag: nx.Graph, x: str, y: str) -> Generator[list[str]
                 not pag.has_edge(u, w) and \
                 hasEndpoint(pag, v, w, Endpoint.CIRCLE, Endpoint.CIRCLE):
                 stack.append((v, w))
+
+def existUncoveredPDPath(pag: nx.Graph, x: str, y: str, z: str) -> bool:
+    visited = {x}
+    stack = [(x, z)]
+
+    while stack:
+        u, v = stack.pop()
+        if v in visited:
+            continue
+        visited.add(u)
+
+        if v == y:
+            return True
+
+        for w in pag.neighbors(v):
+            if w != x and not pag.has_edge(u, w) and isPDEdge(pag, u, v):
+                stack.append((v, w))
+    return False
 
 #=================== skeleton discovery ===================#
 def initialSkeleton(learner: gum.BNLearner,
@@ -240,13 +263,16 @@ def rule0(graph: nx.Graph, sepsets: dict[tuple, set], verbose: bool=False) -> nx
         if xzData[z] == Endpoint.CIRCLE and yzData[z] == Endpoint.CIRCLE:
             if verbose:
                 xEndpoint = xzData[x].value if xzData[x] != Endpoint.ARROW  else "<"
-                print(f"[R0]        '{x}' {xEndpoint}-o '{z}' o-{yzData[y].value} '{y}'\n"
-                        f"            '{z}' not in sepset('{x}', '{y}') = {sepsets.get((x, y), set())}\n"
-                        f"     orient '{x}' {xEndpoint}-> '{z}' <-{yzData[y].value} '{y}'")
+                print(f"[R0]         '{x}' {xEndpoint}-o '{z}' o-{yzData[y].value} '{y}'\n"
+                        f"             '{z}' not in sepset('{x}', '{y}') = {sepsets.get((x, y), set())}\n"
+                        f"      orient '{x}' {xEndpoint}-> '{z}' <-{yzData[y].value} '{y}'")
         
             xzData[z] = Endpoint.ARROW
             yzData[z] = Endpoint.ARROW
     return pag
+
+
+
 
 def rule1(pag: nx.Graph, verbose: bool=False) -> bool:
     hasChange = False
@@ -264,9 +290,9 @@ def rule1(pag: nx.Graph, verbose: bool=False) -> bool:
 
             if verbose:
                 xEndpoint = xzData[x].value if xzData[x] != Endpoint.ARROW else "<"
-                print(f"[R1]        '{x}' {xEndpoint}-> '{z}' o-{yzData[y].value} '{y}'\n"
-                      f"            '{x}' and '{y}' are not adjacent\n"
-                      f"     orient '{z}' -> '{y}'")
+                print(f"[R1]         '{x}' {xEndpoint}-> '{z}' o-{yzData[y].value} '{y}'\n"
+                      f"             '{x}' and '{y}' are not adjacent\n"
+                      f"      orient '{z}' -> '{y}'")
             
             yzData[z] = Endpoint.TAIL
             yzData[y] = Endpoint.ARROW
@@ -292,9 +318,9 @@ def rule2(pag: nx.Graph, verbose: bool=False) -> bool:
             if verbose:
                 xEndpointFromxz = xzData[x].value if xzData[x] != Endpoint.ARROW else "<"
                 xEndpointFromxy = xyData[x].value if xyData[x] != Endpoint.ARROW else "<"
-                print(f"[R2]        '{x}' {xEndpointFromxz}-> '{z}' {yzData[z].value}-{yzData[y].value} '{y}'\n"
-                      f"            '{x}' {xEndpointFromxy}-o '{y}'\n"
-                      f"     orient '{x}' {xEndpointFromxy}-> '{y}'")
+                print(f"[R2]         '{x}' {xEndpointFromxz}-> '{z}' {yzData[z].value}-{yzData[y].value} '{y}'\n"
+                      f"             '{x}' {xEndpointFromxy}-o '{y}'\n"
+                      f"      orient '{x}' {xEndpointFromxy}-> '{y}'")
             
             xyData[y] = Endpoint.ARROW
             hasChange = True
@@ -329,11 +355,11 @@ def rule3(pag: nx.Graph, verbose: bool=False) -> bool:
                 if verbose:
                     xEndpointFromxz = xzData[x].value if xzData[x] != Endpoint.ARROW else "<"
                     xEndpointFromxv = xvData[x].value if xvData[x] != Endpoint.ARROW else "<"
-                    print(f"[R3]        '{x}' {xEndpointFromxz}-> '{z}' <-{yzData[z].value} '{y}'\n"
-                          f"            '{x}' {xEndpointFromxv}-> '{v}' <-{yvData[v].value} '{y}'\n"
-                          f"            '{z}' o-{zvData[v].value} '{v}'\n"
-                          f"            '{x}' and '{y}' are not adjacent\n"
-                          f"     orient '{z}' <-{zvData[v].value} '{v}'")
+                    print(f"[R3]         '{x}' {xEndpointFromxz}-> '{z}' <-{yzData[z].value} '{y}'\n"
+                          f"             '{x}' {xEndpointFromxv}-> '{v}' <-{yvData[v].value} '{y}'\n"
+                          f"             '{z}' o-{zvData[v].value} '{v}'\n"
+                          f"             '{x}' and '{y}' are not adjacent\n"
+                          f"      orient '{z}' <-{zvData[v].value} '{v}'")
                 zvData[z] = Endpoint.ARROW
                 hasChange = True
     return hasChange
@@ -356,19 +382,19 @@ def rule4(pag: nx.Graph, sepsets: dict[tuple, set], verbose: bool=False) -> bool
             if path is not None:
                 hasChange = True
                 if verbose:
-                    print(f"[R4]        '{x}' <-{xzData[z].value} '{z}' o-{yzData[y].value} '{y}'\n"
-                          f"            '{x}' -> '{y}'\n"
-                          f"            find discriminating path = {path}")
+                    print(f"[R4]         '{x}' <-{xzData[z].value} '{z}' o-{yzData[y].value} '{y}'\n"
+                          f"             '{x}' -> '{y}'\n"
+                          f"             find discriminating path = {path}")
                 
                 if z not in sepsets.get((path[-1], y), set()):
                     if verbose:
-                        print(f"     orient '{x}' <-> '{z}' <-> '{y}'")
+                        print(f"      orient '{x}' <-> '{z}' <-> '{y}'")
                     xzData[z] = Endpoint.ARROW
                     yzData[z] = Endpoint.ARROW
                     yzData[y] = Endpoint.ARROW
                 else:
                     if verbose:
-                        print(f"     orient '{z}' -> '{y}'")
+                        print(f"      orient '{z}' -> '{y}'")
                     yzData[z] = Endpoint.TAIL
                     yzData[y] = Endpoint.ARROW
     return hasChange
@@ -385,21 +411,27 @@ def rule5(pag: nx.Graph, verbose: bool=False) -> bool:
 
         # Path : x o-o u o-o ... o-o v o-o y.
         for path in getUncoveredCirclePath(pag, x, y):
-            print(path)
-            if verbose:
-                print(f"[R5]        '{x}' o-o '{y}'\n"
-                      f"            find uncovered circle path = {path}\n"
-                      f"     orient '{x}' - '{y}' and '{x}' - ... - '{y}'")
-            hasChange = True
+            # path.length > 3
+            # u and y are not adjacent.
+            # v and x are not adjacent.
+            if  path[1] != path[-2] and \
+                not pag.has_edge(path[1], path[-1]) and \
+                not pag.has_edge(path[0], path[-2]):
 
-            # Orient x o-o u o-o ... o-o v o-o y as x - u - ... - v - y.
-            for i in range(len(path) - 1):
-                u, v = path[i], path[i + 1]
-                pag[u][v][u] = Endpoint.TAIL
-                pag[u][v][v] = Endpoint.TAIL
-            
-            xyData[x] = Endpoint.TAIL
-            xyData[y] = Endpoint.TAIL
+                if verbose:
+                    print(f"[R5]         '{x}' o-o '{y}'\n"
+                        f"             find uncovered circle path = {path}\n"
+                        f"      orient '{x}' - '{y}' and '{x}' - ... - '{y}'")
+                hasChange = True
+
+                # Orient x o-o u o-o ... o-o v o-o y as x - u - ... - v - y.
+                for i in range(len(path) - 1):
+                    u, v = path[i], path[i + 1]
+                    pag[u][v][u] = Endpoint.TAIL
+                    pag[u][v][v] = Endpoint.TAIL
+                
+                xyData[x] = Endpoint.TAIL
+                xyData[y] = Endpoint.TAIL
     return hasChange
 
 def rule6(pag: nx.Graph, verbose: bool=False) -> bool:
@@ -411,8 +443,8 @@ def rule6(pag: nx.Graph, verbose: bool=False) -> bool:
         if  xzData[x] == Endpoint.TAIL and xzData[z] == Endpoint.TAIL and \
             yzData[z] == Endpoint.CIRCLE:
             if verbose:
-                print(f"[R6]        '{x}' - '{z}' o-{yzData[y].value} '{y}'\n"
-                      f"     orient '{z}' -{yzData[y].value} '{y}'")
+                print(f"[R6]         '{x}' - '{z}' o-{yzData[y].value} '{y}'\n"
+                      f"      orient '{z}' -{yzData[y].value} '{y}'")
             
             yzData[z] = Endpoint.TAIL
             hasChange = True
@@ -430,9 +462,9 @@ def rule7(pag: nx.Graph, verbose: bool=False) -> bool:
         if  xzData[x] == Endpoint.TAIL and xzData[z] == Endpoint.CIRCLE and \
             yzData[z] == Endpoint.CIRCLE:
             if verbose:
-                print(f"[R7]        '{x}' -o '{z}' o-{yzData[y].value} '{y}'\n"
-                      f"            '{x}' and '{y}' are not adjacent\n"
-                      f"     orient '{z}' -{yzData[y].value} '{y}'")
+                print(f"[R7]         '{x}' -o '{z}' o-{yzData[y].value} '{y}'\n"
+                      f"             '{x}' and '{y}' are not adjacent\n"
+                      f"      orient '{z}' -{yzData[y].value} '{y}'")
             
             yzData[z] = Endpoint.TAIL
             hasChange = True
@@ -457,16 +489,62 @@ def rule8(pag: nx.Graph, verbose: bool=False) -> bool:
             yzData[z] == Endpoint.TAIL and yzData[y] == Endpoint.ARROW and \
             xyData[x] == Endpoint.CIRCLE and xyData[y] == Endpoint.ARROW:
             if verbose:
-                print(f"[R8]        '{x}' -{xzData[z].value} '{z}' -> '{y}'"
-                      f"            '{x}' o-> '{y}'"
-                      f"     orient '{x}' -> '{y}'")
+                print(f"[R8]         '{x}' -{xzData[z].value} '{z}' -> '{y}'"
+                      f"             '{x}' o-> '{y}'"
+                      f"      orient '{x}' -> '{y}'")
 
             xyData[x] = Endpoint.TAIL
             hasChange = True
     return hasChange
 
 def rule9(pag: nx.Graph, verbose: bool=False) -> bool:
-    raise NotImplemented()
+    hasChange = False
+    for x, y, xyData in pag.edges(data=True):
+        if not hasEndpoint(pag, x, y, Endpoint.CIRCLE, Endpoint.ARROW):
+            continue
+
+        for z in pag.neighbors(x):
+            if  not pag.has_edge(z, y) and isPDEdge(pag, x, z) and \
+                existUncoveredPDPath(pag, x, y, z):
+                    if verbose:
+                        print(f"[R9]         '{x}' o-> '{y}'\n"
+                              f"             exist uncovered p.d. path = ('{x}', '{z}', ..., '{y}')\n"
+                              f"      orient '{x}' -> '{y}'")
+                    xyData[x] = Endpoint.TAIL
+                    hasChange = True
+                    break
+    return hasChange
 
 def rule10(pag: nx.Graph, verbose: bool=False) -> bool:
-    raise NotImplemented()
+    hasChange = False
+    for x, y, xyData in pag.edges(data=True):
+        if not hasEndpoint(pag, x, y, Endpoint.CIRCLE, Endpoint.ARROW):
+            continue
+
+        neighborsXMinusY = set(pag.neighbors(x)) - {y}
+
+        done = False
+        for u, v in permutations(pag.neighbors(y), 2):
+            # We assume u -> y <- v.
+            if  not hasEndpoint(pag, u, y, Endpoint.TAIL, Endpoint.ARROW) or \
+                not hasEndpoint(pag, v, y, Endpoint.TAIL, Endpoint.ARROW):
+                continue
+
+            for uPrime, vPrime in combinations(neighborsXMinusY, 2):
+                if  isPDEdge(pag, x, uPrime) and isPDEdge(pag, x, vPrime) and \
+                    existUncoveredPDPath(pag, x, u, uPrime) and existUncoveredPDPath(pag, x, v, vPrime):
+                    if verbose:
+                        print(f"[R10]        '{x}' o-> '{y}'\n"
+                              f"             '{u}' -> '{y}' <- '{v}'\n"
+                              f"             exist uncovered p.d. path = ('{x}', '{u}', ..., '{uPrime}')\n"
+                              f"             exist uncovered p.d. path = ('{x}', '{v}', ..., '{vPrime}')\n"
+                              f"      orient '{x}' -> '{y}'")
+                    xyData[x] = Endpoint.TAIL
+                    hasChange = True
+
+                    done = True
+                if done:
+                    break
+            if done:
+                break
+    return hasChange
